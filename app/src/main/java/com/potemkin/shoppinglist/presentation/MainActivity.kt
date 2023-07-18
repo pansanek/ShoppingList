ed22a09be683a966218dc167e69c0d551a0d6496
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-18 15:46:25 +0300

    domain layer with repository

--- app/src/main/java/com/potemkin/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/potemkin/shoppinglist/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.potemkin.shoppinglist
+package com.potemkin.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.potemkin.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
