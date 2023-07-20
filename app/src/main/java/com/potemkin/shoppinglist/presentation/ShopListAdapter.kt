8910945ce5621e1476c1e5c46903a99aba263fc8
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-20 15:31:40 +0300

    Начало реализации RecyclerView

---
+++ app/src/main/java/com/potemkin/shoppinglist/presentation/ShopListAdapter.kt
@@ -0,0 +1,36 @@
+package com.potemkin.shoppinglist.presentation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.potemkin.shoppinglist.R
+import com.potemkin.shoppinglist.domain.ShopItem
+
+class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
+
+    val list = listOf<ShopItem>()
+
+    class ShopItemViewHolder(val view: View) :RecyclerView.ViewHolder(view){
+        val tvName=view.findViewById<TextView>(R.id.tv_name)
+        val tvCount=view.findViewById<TextView>(R.id.tv_count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled,parent,false)
+        return ShopItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        val shopItem=list[position]
+        holder.tvName.text=shopItem.name
+        holder.tvCount.text=shopItem.count.toString()
+        holder.view.setOnLongClickListener { true }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+}
