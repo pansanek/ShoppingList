50856d7baa85264a4365dd84748104d0f31f9eea
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-20 15:54:22 +0300

    Разобран баг с переопределением холдеров

--- app/src/main/java/com/potemkin/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/potemkin/shoppinglist/presentation/ShopListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.potemkin.shoppinglist.R
@@ -11,7 +12,11 @@ import com.potemkin.shoppinglist.domain.ShopItem
 
 class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field=value
+            notifyDataSetChanged()
+        }
 
     class ShopItemViewHolder(val view: View) :RecyclerView.ViewHolder(view){
         val tvName=view.findViewById<TextView>(R.id.tv_name)
@@ -19,18 +24,31 @@ class ShopListAdapter: RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_disabled,
+            parent,
+            false
+        )
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        val shopItem=list[position]
-        holder.tvName.text=shopItem.name
-        holder.tvCount.text=shopItem.count.toString()
+        val shopItem=shopList[position]
         holder.view.setOnLongClickListener { true }
+        if (shopItem.enabled){
+            holder.tvName.text=shopItem.name
+            holder.tvCount.text=shopItem.count.toString()
+            holder.tvName.setTextColor(ContextCompat.getColor(holder.view.context,android.R.color.holo_red_dark))
+        }
+    }
+
+    override fun onViewRecycled(holder: ShopItemViewHolder) {
+        super.onViewRecycled(holder)
+        holder.tvName.text=""
+        holder.tvCount.text=""
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return shopList.size
     }
 }
