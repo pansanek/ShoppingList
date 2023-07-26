3cc1d5c6044c3637e0aad6d9558d25e54f293586
SashaPotemkin <alex.potjomkin@gmail.com>
2023-07-26 14:36:10 +0300

    Создание базы данных

---
+++ app/src/main/java/com/potemkin/shoppinglist/data/ShopListDao.kt
@@ -0,0 +1,24 @@
+package com.potemkin.shoppinglist.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.potemkin.shoppinglist.domain.ShopItem
+
+@Dao
+interface ShopListDao {
+
+    @Query("SELECT * FROM shop_items")
+    fun getShopList(): LiveData<List<ShopItemDBModel>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addShopItem(shopItemDBModel: ShopItemDBModel)
+
+    @Query("DELETE FROM shop_items WHERE id=:shopItemId")
+    fun deleteShopItem(shopItemId:Int)
+
+    @Query("SELECT * FROM shop_items WHERE id=:shopItemId LIMIT 1")
+    fun getShopItem(shopItemId:Int): ShopItemDBModel
+}
