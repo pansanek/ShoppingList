3745e998f2facd373731e62986ff51ac508d729f
SashaPotemkin <alex.potjomkin@gmail.com>
2023-08-03 15:05:08 +0300

    UriMatcher

--- app/src/main/java/com/potemkin/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/potemkin/shoppinglist/data/ShopListProvider.kt
@@ -2,11 +2,16 @@ package com.potemkin.shoppinglist.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShopListProvider: ContentProvider() {
+
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI("com.potemkin.shoppinglist","shop_items", GET_SHOP_ITEMS_QUERY)
+    }
     override fun onCreate(): Boolean {
         return true
     }
@@ -18,7 +23,13 @@ class ShopListProvider: ContentProvider() {
         p3: Array<out String>?,
         p4: String?
     ): Cursor? {
-        Log.d("ShopListProvider","Query $p0")
+        val code = uriMatcher.match(p0)
+        when(code) {
+            GET_SHOP_ITEMS_QUERY-> {
+
+            }
+        }
+        Log.d("ShopListProvider","Query $p0 code  $code")
         return null
     }
 
@@ -36,5 +47,9 @@ class ShopListProvider: ContentProvider() {
 
     override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object{
+        private const val GET_SHOP_ITEMS_QUERY = 100
     }
 }
